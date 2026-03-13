ed3c62d1699c1e8646166c532d1a709b523d5b4e
ebenois <ebenois@ensc.fr>
2026-03-13 16:09:17 +0100

    Correction bug

--- Mandats-Corruptions/src/main.jsx
+++ Mandats-Corruptions/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import PoliticiansPage from './pages/PoliticiansPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PoliticiansPage />
   </StrictMode>,
 )
